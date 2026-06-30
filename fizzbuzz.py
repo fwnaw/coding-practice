b24ce2e95232a9a42bb85a0cd7c95d38777cb2a6
Nawfel <nawfeltahar@gmail.com>
2026-06-30 23:34:39 +0100

    Add fizzbuzz solution

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
+  
+
+for i in range(1, 21):
+    fizzbuzz(i)
